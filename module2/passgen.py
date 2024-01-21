78a4b8c9571a3708ef279962c2cd5a28ffc0b9df
rich98 <51236157+rich98@users.noreply.github.com>
2024-01-21 14:15:25 +0000

    Add files via upload

---
+++ module2/passgen.py
@@ -0,0 +1,10 @@
+import string
+import random
+
+def generate_password(length):
+    all_characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(all_characters) for _ in range(length))
+    return password
+
+# Generate a 12-character password
+print(generate_password(12))
