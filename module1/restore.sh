e7f836bc8cdd991b318967c1b713a38655b62db7
rich98 <51236157+rich98@users.noreply.github.com>
2024-01-27 09:51:46 +0000

    Update restore.sh

--- module1/restore.sh
+++ module1/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the decryption password
-decryption_password="e4KfInoequa>nGirpgseh(Vf"
+decryption_password="e4KfInoequanGirpgsehf"
 
 # GPG decryption settings with a password file
 password_file="/usr/local/bin/restore_script/password_file.txt"
