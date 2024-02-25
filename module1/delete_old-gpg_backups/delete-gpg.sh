272605602f75bbcae8b1831d13f439d5e34d25e6
rich98 <51236157+rich98@users.noreply.github.com>
2024-02-25 18:50:28 +0000

    Update delete-gpg.sh

--- module1/delete_old-gpg_backups/delete-gpg.sh
+++ module1/delete_old-gpg_backups/delete-gpg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Directory where the .gpg files are located
-DIR="/path/to/your/directory"
+DIR="/backup/bkmp/"
 
 # Find and delete .gpg files older than 5 days
 find "$DIR" -name "*.gpg" -type f -mtime +5 -exec rm -f {} \;
